f334cf3471e4a0f9403962e43b0fc4f0cae2b0d2
py-shen <py-shen@users.noreply.github.com>
2018-04-19 14:29:49 +0800

    version.0

--- Weather.py
+++ Weather.py
@@ -2,6 +2,7 @@
 
 from urllib import request
 import gzip
+
 cityname = request.quote(input('请输入要查的城市名称： ') + '市')
 # print(cityname)
 content = request.urlopen('http://wthrcdn.etouch.cn/weather_mini?city=%s' % cityname).read()
@@ -9,8 +10,12 @@ try:
 	web = gzip.decompress(content).decode('utf8')
 except:
 	web = content.decode('utf8')
-print(web)
-result = {}
+if web == ('{"status":1002,"desc":"invilad-citykey"}'):
+	print('error')
+else:
+	print(web)
+
+
 
 
 
